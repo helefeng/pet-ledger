6138b448461a3267e04254409e1ec943221c5923
xfeng <17607644285@163.com>
2026-03-26 10:31:51 +0800

    fix: set vite base to root for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/pet-ledger/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
